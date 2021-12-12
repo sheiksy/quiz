93010aff59e5b747c7f0214054797c6ec993be2e
sheiksy <94904800+sheiksy@users.noreply.github.com>
2021-12-12 15:16:47 +0900

    Add: Determine the correct answer

--- app/controllers/quiz.go
+++ app/controllers/quiz.go
@@ -3,13 +3,18 @@ package controllers
 import (
 	"fmt"
 	"quiz/app/models"
-	"time"
 )
+
+var answer int
 
 func Quiz() {
 	num1 := models.Numerics()
 	num2 := models.Numerics()
 	fmt.Printf("%d + %d = ", num1, num2)
-	time.Sleep(time.Second * 3)
-	fmt.Println(num1 + num2)
+	fmt.Scan(&answer)
+	if answer == (num1 + num2) {
+		fmt.Println("Correct!")
+	} else {
+		fmt.Printf("The correct answer is %d \n", num1+num2)
+	}
 }
